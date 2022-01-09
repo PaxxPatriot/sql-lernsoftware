b467cb47d658816a99e9f53ebd420994a38ae175
Sebastian Adam <sebastian.adam2000@googlemail.com>
2022-01-09 16:36:56 +0100

    Signed-off-by: Sebastian Adam <sebastian.adam2000@googlemail.com>

--- src/main/java/de/dhbw/studienarbeit/sqllernsoftware/HelloController.java
+++ src/main/java/de/dhbw/studienarbeit/sqllernsoftware/HelloController.java
@@ -1,5 +1,6 @@
 package de.dhbw.studienarbeit.sqllernsoftware;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -8,7 +9,14 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
+    private Label testText;
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void handleNewClick(ActionEvent actionEvent) {
+            testText.setText("Test1");
     }
 }
