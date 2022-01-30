7bdd4e3ab603e65edd4776c5892999104fb481e0
sebastian.adam <sebastian.adam@kit.edu>
2022-01-30 17:46:11 +0100

    dynamic creation of menuitems in menubar

--- src/main/java/de/dhbw/studienarbeit/sqllernsoftware/HelloApplication.java
+++ src/main/java/de/dhbw/studienarbeit/sqllernsoftware/HelloApplication.java
@@ -18,7 +18,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mockup.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Hello!");
+        stage.setTitle("SQL-Lernsoftware");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/de/dhbw/studienarbeit/sqllernsoftware/HelloController.java
+++ src/main/java/de/dhbw/studienarbeit/sqllernsoftware/HelloController.java
@@ -1,12 +1,31 @@
 package de.dhbw.studienarbeit.sqllernsoftware;
 
+import de.dhbw.studienarbeit.sqllernsoftware.backend.objekte.Aufgabenkollektion;
+import de.dhbw.studienarbeit.sqllernsoftware.backend.objekte.Lektion;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import de.dhbw.studienarbeit.sqllernsoftware.persistence.AppdataController;
+import javafx.scene.control.MenuItem;
+
+import java.util.List;
 
 public class HelloController {
+    private final AppdataController appdataController = new AppdataController();
+
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Menu lectureMenu;
+
+    @FXML
+    private Menu exerciseMenu;
+
+    @FXML
+    private Menu testMenu;
+
 
     @FXML
     private Label testText;
@@ -16,7 +35,21 @@ public class HelloController {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 
-    public void handleNewClick(ActionEvent actionEvent) {
-            testText.setText("Test1");
+    @FXML
+    public void initialize() {
+        List<Lektion> lektionen = appdataController.getAllLektion();
+        List<Aufgabenkollektion> aufgabenkollektionen = appdataController.getAllAufgabenkollektion();
+        for (Lektion lektion : lektionen) {
+            MenuItem menuItem = new MenuItem(""+lektion.getTitel());
+            lectureMenu.getItems().add(menuItem);
+        }
+
+        for (Aufgabenkollektion kollektion : aufgabenkollektionen) {
+            MenuItem menuItem = new MenuItem(kollektion.getTitel());
+            exerciseMenu.getItems().add(menuItem);
+        }
+
     }
+
+
 }
