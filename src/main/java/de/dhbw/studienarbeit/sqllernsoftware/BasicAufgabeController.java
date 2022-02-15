f87fd4ec425733301408c518e148cec4d5c6d271
sebastian.adam <sebastian.adam@kit.edu>
2022-02-15 15:31:06 +0100

    finished switching mechanic between different fxml pages

--- src/main/java/de/dhbw/studienarbeit/sqllernsoftware/BasicAufgabeController.java
+++ src/main/java/de/dhbw/studienarbeit/sqllernsoftware/BasicAufgabeController.java
@@ -1,4 +1,34 @@
 package de.dhbw.studienarbeit.sqllernsoftware;
 
+import de.dhbw.studienarbeit.sqllernsoftware.backend.objekte.Aufgabenkollektion;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+
 public class BasicAufgabeController {
+
+    private Aufgabenkollektion exerciseCollection;
+
+    @FXML
+    private Label exerciseTitel;
+
+    @FXML
+    private Label exerciseDescription;
+
+
+    public void initialize() throws IOException {
+        exerciseTitel.setText(exerciseCollection.getTitel());
+        exerciseDescription.setText(exerciseCollection.getBeschreibung());
+
+    }
+
+    public void setExerciseCollection(Aufgabenkollektion collection) {
+        this.exerciseCollection = collection;
+        System.out.println("Successfully set collection!");
+    }
+
+    public Aufgabenkollektion getExerciseCollection() {
+        return this.exerciseCollection;
+    }
 }
--- src/main/java/de/dhbw/studienarbeit/sqllernsoftware/HelloController.java
+++ src/main/java/de/dhbw/studienarbeit/sqllernsoftware/HelloController.java
@@ -2,10 +2,12 @@ package de.dhbw.studienarbeit.sqllernsoftware;
 
 import de.dhbw.studienarbeit.sqllernsoftware.backend.objekte.Aufgabenkollektion;
 import de.dhbw.studienarbeit.sqllernsoftware.backend.objekte.Lektion;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -17,44 +19,31 @@ import javafx.stage.Stage;
 import org.hibernate.tool.schema.Action;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HelloController {
     private final AppdataController appdataController = new AppdataController();
-
-    @FXML
-    private Parent menuBar;
-
+    private List<Lektion> lectures;
+    private List<Aufgabenkollektion> exercisecollectionList;
 
     @FXML
     private VBox root;
 
-    @FXML
-    private Label welcomeText;
 
     @FXML
     private Menu lectureMenu;
 
+
     @FXML
     private Menu exerciseMenu;
 
-    @FXML
-    private Menu testMenu;
-
-
-    @FXML
-    private Label testText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     public void initialize() throws IOException {
-        List<Lektion> lektionen = appdataController.getAllLektion();
-        List<Aufgabenkollektion> aufgabenkollektionen = appdataController.getAllAufgabenkollektion();
-        for (Lektion lektion : lektionen) {
+        lectures = appdataController.getAllLektion();
+        exercisecollectionList = appdataController.getAllAufgabenkollektion();
+        for (Lektion lektion : lectures) {
             MenuItem menuItem = new MenuItem(""+lektion.getTitel());
             menuItem.setOnAction(e -> {
                 try {
@@ -66,7 +55,7 @@ public class HelloController {
             lectureMenu.getItems().add(menuItem);
         }
 
-        for (Aufgabenkollektion kollektion : aufgabenkollektionen) {
+        for (Aufgabenkollektion kollektion : exercisecollectionList) {
             MenuItem menuItem = new MenuItem(kollektion.getTitel());
             menuItem.setOnAction(e -> {
                 try {
@@ -82,10 +71,31 @@ public class HelloController {
 
     public void gotoExercise(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("basic_aufgabe.fxml"));
+        //Scene AufgabenManagerScene = new Scene(AufgabenManager, 1280, 700);
+        BasicAufgabeController exerciseController = new BasicAufgabeController();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        for (Aufgabenkollektion collection : exercisecollectionList) {
+            if (collection.getTitel().equals(menuItem.getText())) {
+                exerciseController.setExerciseCollection(collection);
+            }
+        }
+        loader.setController(exerciseController);
         Parent AufgabenManager = loader.load();
-        Scene AufgabenManagerScene = new Scene(AufgabenManager, 1280, 700);
+
+        List<Node> nodesToDelete = new ArrayList<>();
+        for (Object o : root.getChildren()) {
+            Node node = (Node) o;
+            System.out.println(node.getId());
+            if (!node.getId().equals("menuBar")) {
+                nodesToDelete.add(node);
+            }
+        }
+        for (Node node : nodesToDelete) {
+            root.getChildren().remove(node);
+        }
 
         root.getChildren().add(AufgabenManager);
+        //exerciseController.loadPage();
 
         //Stage window = (Stage) menuBar.getScene().getWindow();
         //window.setScene(AufgabenManagerScene);
@@ -94,9 +104,30 @@ public class HelloController {
 
     public void gotoLecture(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("basic_lecture.fxml"));
-        Parent LektionenManager = loader.load();
-        Scene LektionenManagerScene = new Scene(LektionenManager, 1280, 700);
+        BasicLectureController lectureController = new BasicLectureController();
+        MenuItem menuItem = (MenuItem) event.getSource();
+        for (Lektion lecture : lectures) {
+            if (lecture.getTitel().equals(menuItem.getText())) {
+                lectureController.setLecture(lecture);
+            }
+        }
 
+        loader.setController(lectureController);
+        Parent LektionenManager = loader.load();
+
+        List<Node> nodesToDelete = new ArrayList<>();
+        for (Object o : root.getChildren()) {
+            Node node = (Node) o;
+            System.out.println(node.getId());
+            if (!node.getId().equals("menuBar")) {
+                nodesToDelete.add(node);
+            }
+        }
+        for (Node node : nodesToDelete) {
+            root.getChildren().remove(node);
+        }
+
+        root.getChildren().add(LektionenManager);
         //Stage window = (Stage) root.getScene().getWindow();
         //window.setScene(LektionenManagerScene);
         //window.show();
