174ed5fb3a212a5aa83a1a2e0825d1fde69df04d
PaxxPatriot <34141101+PaxxPatriot@users.noreply.github.com>
2022-02-17 14:56:31 +0100

    Split execute methods Delete temporary databases

--- src/main/java/de/dhbw/studienarbeit/sqllernsoftware/datenbasis/DatenbasisController.java
+++ src/main/java/de/dhbw/studienarbeit/sqllernsoftware/datenbasis/DatenbasisController.java
@@ -10,14 +10,17 @@ import java.util.logging.Logger;
 public class DatenbasisController {
     private static final Logger logger = Logger.getLogger("de.dhbw.studienarbeit.sqllernsoftware.datenbasis.DatenbasisController");
 
-    private static ResultSet executeOnDatabase(String query, String datenbankPfad) throws SQLException {
+    private static ResultSet executeQueryOnDatabase(String query, String datenbankPfad) throws SQLException {
         try (Connection conn = DriverManager.getConnection(datenbankPfad)) {
             Statement stmt = conn.createStatement();
-            if (query.startsWith("DELETE") || query.startsWith("UPDATE")) {
-                stmt.executeUpdate(query);
-                return null;
-            }
             return stmt.executeQuery(query);
+        }
+    }
+
+    private static int executeUpdateOnDatabase(String query, String datenbankPfad) throws SQLException {
+        try (Connection conn = DriverManager.getConnection(datenbankPfad)) {
+            Statement stmt = conn.createStatement();
+            return stmt.executeUpdate(query);
         }
     }
 
@@ -25,8 +28,8 @@ public class DatenbasisController {
         ResultSet[] abfrageUndLoesung = new ResultSet[2];
         String url = String.format("jdbc:sqlite:%s", datenbankPfad);
         try {
-            abfrageUndLoesung[0] = executeOnDatabase(query, url);
-            abfrageUndLoesung[1] = executeOnDatabase(loesung, url);
+            abfrageUndLoesung[0] = executeQueryOnDatabase(query, url);
+            abfrageUndLoesung[1] = executeQueryOnDatabase(loesung, url);
             return abfrageUndLoesung;
         } catch (SQLException e) {
             logger.warning(e.getMessage());
@@ -34,17 +37,22 @@ public class DatenbasisController {
         return null;
     }
 
-    public static ResultSet[] executeAbfrageUndMusterloesungOnCopyOfDatenbasis(String query, String loesung) {
-        ResultSet[] abfrageUndLoesung = new ResultSet[2];
+    public static int[] executeAbfrageUndMusterloesungOnCopyOfDatenbasis(String query, String loesung) {
+        int[] abfrageUndLoesung = new int[2];
 
         String url_user = copyDatenbasis();
         String url_muster = copyDatenbasis();
         try {
-            abfrageUndLoesung[0] = executeOnDatabase(query, url_user);
-            abfrageUndLoesung[1] = executeOnDatabase(loesung, url_muster);
+            abfrageUndLoesung[0] = executeUpdateOnDatabase(query, url_user);
+            abfrageUndLoesung[1] = executeUpdateOnDatabase(loesung, url_muster);
             return abfrageUndLoesung;
         } catch (SQLException e) {
             logger.warning(e.getMessage());
+        }
+        finally {
+            // Delete temporary copies of Datenbasis
+            new File(url_user.substring(12)).delete();
+            new File(url_muster.substring(12)).delete();
         }
         return null;
     }
